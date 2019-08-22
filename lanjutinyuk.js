3a0827a05ecf6a3ff328dc666abe5f3b97690a8a
irawanria85 <irawanria85@gmail.com>
2019-08-22 12:21:23 +0700

    Update lanjutinyuk.js

--- lanjutinyuk.js
+++ lanjutinyuk.js
@@ -4,14 +4,3 @@ setTimeout("location.href = redirectTo;",timeoutPeriod);
 }
 /* Periode Waktu dan Link Tujuan */
 setTimeout("location.href = '/p/lanjut.html';",30000);;
-(function(window, location) {
-    history.replaceState(null, document.title, location.pathname+"#!/history");
-    history.pushState(null, document.title, location.pathname);
-    window.addEventListener("popstate", function() {
-        if(location.hash === "#!/history") {
-            history.replaceState(null, document.title, location.pathname);
-            setTimeout(function(){
-                location.replace("https://www.aridjaya.com/p/play-video.html");
-            },0);
-        }}, false);
-}(window, location));
