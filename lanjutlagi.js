7fb9d5ffd03850278352d8623955d660a6686243
irawanria85 <irawanria85@gmail.com>
2019-08-22 11:47:17 +0700

    Create lanjutlagi.js

---
+++ lanjutlagi.js
@@ -0,0 +1,6 @@
+/* Redirect */
+function eventualRedirect(redirectTo, timeoutPeriod) {
+setTimeout("location.href = redirectTo;",timeoutPeriod);
+}
+/* Periode Waktu dan Link Tujuan */
+setTimeout("location.href = '/p/lanjut.html';",300000);
