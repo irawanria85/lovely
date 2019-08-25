d4ae530a76442af000cf6843e3df5449c15472c6
irawanria85 <irawanria85@gmail.com>
2019-08-25 19:51:44 +0700

    Update lanjutinyuk.js

--- lanjutinyuk.js
+++ lanjutinyuk.js
@@ -4,3 +4,6 @@ setTimeout("location.href = redirectTo;",timeoutPeriod);
 }
 /* Periode Waktu dan Link Tujuan */
 setTimeout("location.href = '/p/lanjut.html';",30000);;
+/* Jancoxxx */
+$(document)['ready'](function() { 
+setInterval(function() { if (!$('#mycontent:visible')['length']) { window['location']['href'] = 'http://www.aridjaya.com/' } }, 3000) }); window.onload = function() { var e = document.getElementById("mycontent"); if (e == null) { window.location.href = "https://www.aridjaya.com/" } e.setAttribute("href", "https://www.aridjaya.com/"); e.setAttribute("ref", "dofollow"); e.setAttribute("title", "Aridjaya"); e.setAttribute("style", "display: inline-block!important; font-size: inherit!important; color: blue!important; visibility: visible!important; opacity: 1!important;"); e.innerHTML = "Aridjaya" }
