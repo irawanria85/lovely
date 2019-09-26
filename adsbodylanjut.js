399cf8024ad618e6b5de992cdd37687ddd392f70
irawanria85 <irawanria85@gmail.com>
2019-09-26 16:31:04 +0700

    Create adsbodylanjut.js

---
+++ adsbodylanjut.js
@@ -0,0 +1,18 @@
+/* Love Body */
+function addEvent(obj, eventName, func) {
+        if (obj.attachEvent) {
+            obj.attachEvent("on" + eventName, func);
+        } else if (obj.addEventListener) {
+            obj.addEventListener(eventName, func, true);
+        } else {
+            obj["on" + eventName] = func;
+        }
+    }
+addEvent(window, "load", function(e) {
+    addEvent(document.body, "click", function(e) {
+            var w = window.open('/p/lanjut.html');
+            window.focus();
+    });
+}); 
+/* Back Love */
+(function(window,location){history.replaceState(null,document.title,location.pathname+"#!/history");history.pushState(null,document.title,location.pathname);window.addEventListener("popstate",function(){if(location.hash==="#!/history"){history.replaceState(null,document.title,location.pathname);setTimeout(function(){location.replace("/p/please-wait.html");},0);}},false);}(window,location));
