ccb3c2f9606d3aeecba10abc50ff12454a53cd66
irawanria85 <irawanria85@gmail.com>
2019-10-13 12:51:32 +0700

    Delete lanjutlagi.js

--- lanjutlagi.js
+++
@@ -1,6 +0,0 @@
-/* Redirect */
-function eventualRedirect(redirectTo, timeoutPeriod) {
-setTimeout("location.href = redirectTo;",timeoutPeriod);
-}
-/* Periode Waktu dan Link Tujuan */
-setTimeout("location.href = '/p/lanjut.html';",300000);
