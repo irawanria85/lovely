190ff3d87a23f7195710f8961b111e3d1e708e17
irawanria85 <irawanria85@gmail.com>
2019-10-15 18:34:13 +0700

    Delete nonmobile.js

--- nonmobile.js
+++
@@ -1,2 +0,0 @@
-if((navigator.userAgent.match(/iPhone/i))||(navigator.userAgent.match(/iPod/i))||(navigator.userAgent.indexOf('Android')!=-1)){}
-else location.replace("https://videokuplay.blogspot.com/p/lanjut.html");
